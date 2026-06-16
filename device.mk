433c8f06cb0db5083b1c7c14536d06488fc66f7d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-17 05:32:09 +0800

    test

--- device.mk
+++ device.mk
@@ -536,6 +536,7 @@ PRODUCT_PACKAGES += \
     WifiResOverlayBlossom
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/sku_dandelion/disable-nfc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sku_dandelion/disable-nfc.xml \
     $(LOCAL_PATH)/configs/sysconfig/sku_angelica/disable-nfc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sku_angelica/disable-nfc.xml \
     $(LOCAL_PATH)/configs/sysconfig/sku_angelicain/disable-nfc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sku_angelicain/disable-nfc.xml \
     $(LOCAL_PATH)/configs/sysconfig/sku_cattail/disable-nfc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sku_cattail/disable-nfc.xml
