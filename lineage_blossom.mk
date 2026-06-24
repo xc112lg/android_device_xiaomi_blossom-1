722f1cb35a598f6e09a5cff9db689cfdebb95230
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-24 18:50:37 +0800

    Update lineage_blossom.mk

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -33,8 +33,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 -include vendor/lineage-priv/keys/keys.mk
 
 #Enable Blur
-TARGET_ENABLE_BLUR := false
-TARGET_SUPPORTS_BLUR := false
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_BLUR := true
 
 #Bomb AudioFx
 BOMB_AUDIOFX := true
