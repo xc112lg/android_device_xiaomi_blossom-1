f239c4e280e64765f68d60d159d8fc6cf7e5028e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-27 08:01:20 +0800

    Update Android.mk

--- audio/Android.mk
+++ audio/Android.mk
@@ -8,48 +8,18 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-LOCAL_PATH := hardware/interfaces/audio/common/all-versions/default/service
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.audio.service.mediatek
-LOCAL_REQUIRED_MODULES := android.hardware.audio.service.mediatek.rc
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_VENDOR_MODULE := true
-LOCAL_MULTILIB := 32
-
-LOCAL_SRC_FILES := \
-    service.cpp
-
-LOCAL_CFLAGS := \
-    -Wall \
-    -Wextra \
-    -Werror
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libbinder \
-    libbinder_ndk \
-    libhidlbase \
-    liblog \
-    libutils \
-    libhardware
-
-include $(BUILD_EXECUTABLE)
-
-# Copy Dolby audio service
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.audio.service.mediatek
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES := proprietary/bin/hw/android.hardware.audio.service.mediatek
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/bin/hw
-include $(BUILD_PREBUILT)
-
+# Dolby Media Player Service Library
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmediaplayerservice
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SRC_FILES := proprietary/lib/libmediaplayerservice.so
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_64 := proprietary/lib/libmediaplayerservice.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libmediaplayerservice.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 include $(BUILD_PREBUILT)
+
+# Note: android.hardware.audio.service.mediatek is already defined in 
+# hardware/interfaces/audio/common/all-versions/default/service
+# Do not redefine it here to avoid duplicate module errors
